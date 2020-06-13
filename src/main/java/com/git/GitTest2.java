f2d093da333104465fc570b6e168715a0abb4f7b
zhaodlchn <2070074697@qq.com>
2020-06-13 18:21:13 +0800

    modify GitTest 2

--- src/main/java/com/git/GitTest2.java
+++ src/main/java/com/git/GitTest2.java
@@ -5,5 +5,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("=== Test Git ===");
 	System.out.println(" Git New ");
+	System.out.println(" Git New 2 ");
     }
 }
