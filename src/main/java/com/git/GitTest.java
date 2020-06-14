97c599859adcfe08dcf625c6fc44800e7ebf8d32
zhaodlchn <2070074697@qq.com>
2020-06-14 23:54:49 +0800

    modify file GitTest.java new user

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
 	System.out.println("=== Test Git 2 ===");
 	System.out.println("=== Test Git 3 ===");
 	System.out.println("=== Test Git 4 ===");
+	System.out.println("=== Test Git 4 new user ===");
     }
 }
 
