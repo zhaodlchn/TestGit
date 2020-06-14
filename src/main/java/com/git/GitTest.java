93efd879b79d4be07a939d286efa36028764245f
zhaodlchn <2070074697@qq.com>
2020-06-14 23:35:56 +0800

    modify file GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("=== Test Git 1 ===");
 	System.out.println("=== Test Git 2 ===");
+	System.out.println("=== Test Git 3 ===");
     }
 }
 
