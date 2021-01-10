0c63fd46b7448fc522ab3c48dfd3a84e615268fe
zhaodlchn <2070074697@qq.com>
2021-01-10 15:47:01 +0800

    1、测试-1； 2、测试异常。

---
+++ src/main/java/com/git/GitTest4.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class GitTest2 {
+
+    public static void main(String[] args) {
+        System.out.println("=== Test Git ===");
+	System.out.println(" Git New ");
+	System.out.println(" Git New 2 ");
+    }
+}
