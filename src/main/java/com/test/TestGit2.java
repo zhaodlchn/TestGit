6fdb5864ee6d328ce11aed1b632c1b9a500a171d
zhaodlchn <49180130+zhaodlchn@users.noreply.github.com>
2021-06-25 00:02:07 +0800

    modify TestGit ONE

--- src/main/java/com/test/TestGit2.java
+++ src/main/java/com/test/TestGit2.java
@@ -4,5 +4,6 @@ public class TestGit2 {
 
     public static void main(String[] args) {
         System.out.println("=== SSTest Git2 ===");
+        System.out.println("=== SSTest Git2 ONE ===");
     }
 }
