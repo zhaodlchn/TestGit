13e60a34b140275679b81cd41cb4c24c6d8d51a1
zhaodlchn <49180130+zhaodlchn@users.noreply.github.com>
2021-06-25 00:09:37 +0800

    modiy TestGit2 TWO

--- src/main/java/com/test/TestGit2.java
+++ src/main/java/com/test/TestGit2.java
@@ -5,5 +5,6 @@ public class TestGit2 {
     public static void main(String[] args) {
         System.out.println("=== SSTest Git2 ===");
         System.out.println("=== SSTest Git2 ONE ===");
+        System.out.println("=== SSTest Git2 TWO ===");
     }
 }
