da76bdd614cd80a68a0979f2a9addad8be290ed3
zhaodlchn <2070074697@qq.com>
2021-07-18 22:29:57 +0800

    Modify File TestGit2.java

--- src/main/java/com/test/TestGit2.java
+++ src/main/java/com/test/TestGit2.java
@@ -5,5 +5,7 @@ public class TestGit2 {
     public static void main(String[] args) {
         System.out.println("=== SSTest Git2 ===");
         System.out.println("=== SSTest Git2 ONE ===");
+        System.out.println("=== SSTest Git2 TWO ===");
+        System.out.println("=== SSTest Git2 FIVE 123 ===");
     }
 }
