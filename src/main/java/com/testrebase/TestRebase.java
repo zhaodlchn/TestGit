67128c1b1471108ab03c0aa28d8c3e10b42573f4
zhaodlchn <2070074697@qq.com>
2022-04-10 22:41:31 +0800

    modify rebase -2

--- src/main/java/com/testrebase/TestRebase.java
+++ src/main/java/com/testrebase/TestRebase.java
@@ -7,6 +7,7 @@ public class TestRebase {
     public static void main(String[] args) {
         System.out.println("=== Test Rebase 1 ===");
 		System.out.println("=== Test Rebase 2 ===");
+		System.out.println("=== Test Rebase 3 ===");
     }
 }
 
